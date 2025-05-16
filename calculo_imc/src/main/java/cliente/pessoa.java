85e5bda5577c82f739becd4ef977a7663db609bb
Luis Felipe B. de Mattos <luisfelipedemattos01@gmail.com>
2025-05-16 17:23:51 -0300

    Calcular o IMC
    Lembre-se de terminar o projeto e atualizar ele.

---
+++ calculo_imc/src/main/java/cliente/pessoa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cliente;
+
+/**
+ *
+ * @author luis.fbmattos
+ */
+public class pessoa {
+   
+  public void pessoa(String nome, double altura, double peso, int idade){
+    
+ 
+       
+        
+        
+    } 
+    
+    
+    
+}
---
+++ calculo_imc/src/main/java/com/mycompany/calculo_imc/Calculo_imc.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculo_imc;
+
+import interfaces.principal;
+
+/**
+ *
+ * @author luis.fbmattos
+ */
+public class Calculo_imc {
+
+    public static void main(String[] args) {
+        principal p = new principal();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+    }
+}
