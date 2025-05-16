da8be17326793fd2b7e6443d8e2f84d58866e622
Luis Felipe B. de Mattos <luisfelipedemattos01@gmail.com>
2025-05-16 17:24:31 -0300

    ComboBox

---
+++ ComboBox/src/main/java/com/mycompany/combobox/ComboBox.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.combobox;
+
+/**
+ *
+ * @author luis.fbmattos
+ */
+public class ComboBox {
+
+    public static void main(String[] args) {
+        janelinha j = new janelinha();
+        j.setVisible(true);
+        j.setLocationByPlatform(true);
+    }
+}
